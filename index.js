ec25aa7e62ee143697e506f9f8cbbe0d3608e820
Tanviras <tanvirasme@gmail.com>
2021-03-19 00:40:52 +0600

    Read of CRUD done

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const bodyParser=require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://arabian:MIxy3w6dFdE4Qz8b@cluster0.pjygh.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+const databaseName='burjAlArab';
+const collections= 'bookings';
+const userName='arabian';
+const password='kRXf0eNYc6pereHt';
+const uri = "mongodb+srv://arabian:kRXf0eNYc6pereHt@cluster0.pjygh.mongodb.net/burjAlArab?retryWrites=true&w=majority";
 
 const app=express();
 
@@ -16,19 +20,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
 const bookings = client.db("burjAlArab").collection("bookings");
-console.log('db connection successfully');
+// console.log('db connection successfully');
 
 
 
   app.post('/addBooking', (req, res) => {
     const newBooking = req.body;
-    console.log(newBooking);
-
-    // bookings.insertOne(newBooking)
-    //     .then(result => {
-    //   console.log(result);
-    // })
+    // console.log(newBooking);
+    bookings.insertOne(newBooking)
+    .then(result => {
+      console.log(result);
+      res.send(result.insertedCount>0);//if it is >0,then something is inserted 
+    })
+     console.log(newBooking);
 })//app.post
+
+
+app.get('/bookings',(req,res)=>{//check http://localhost:5000/bookings
+
+  // console.log(req.query.email);
+  bookings.find({email: req.query.email}) //empty {} means all data has to be retrieved from cluster database
+  // booking.find({}).limit()//limiting how many data we will retrieve from cluster database
+  //booking.find({email: req.query.email})//accessing the 'email'property from database's data
+  
+  .toArray((err,documents)=>{
+      res.send(documents);
+  })
+})
 
 })//client.connect
 
